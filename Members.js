42bf8b22e5a8aea03fa8a4e44ef4ee54f31676bc
Prabhat Ranjan <prabhat5172992@gmail.com>
2021-02-13 20:14:17 +0530

    first commit

---
+++ Members.js
@@ -0,0 +1,32 @@
+const members = [
+    {
+        id: 1,
+        name: 'Prabhat Ranjan',
+        email: 'pranjan@gmail.com',
+        status: 'active',
+        age: 25
+    },
+    {
+        id: 2,
+        name: 'Ajeet Kumar',
+        email: 'ajeet@gmail.com',
+        status: 'inactive',
+        age: 35
+    },
+    {
+        id: 3,
+        name: 'Sanjana Dubey',
+        email: 'sanjan@gmail.com',
+        status: 'active',
+        age: 23,
+    },
+    {
+        id: 4,
+        name: 'Arpit Shukla',
+        email: 'arpit@gmail.com',
+        status: 'inactive',
+        age: 12,
+    }
+]
+
+module.exports = members;
---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+// const logger = require('./middleware/logger');
+const members = require('./Members');
+
+const app = express();
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// })
+
+
+// Init Middleware
+// app.use(logger);
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Body Parse Middleware
+app.use(express.json());
+
+// For form data submission
+app.use(express.urlencoded({ extended: false }))
+
+
+// Homepage Route
+app.get('/', (req, res) => res.render('index', {
+    title: 'Member App',
+    members
+}));
+
+// Set a static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Members api routes
+app.use('/api/members', require('./routes/api/members'));
+
+
+const PORT  = process.env.PORT || 5000;
+
+app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
